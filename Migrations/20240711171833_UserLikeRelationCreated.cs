a293e309d9a1113a25d9175d6cc3d1cc05abcd1d
Edinson Perales <eperalesvi@unprg.edu.pe>
2024-07-11 12:21:08 -0500

    User-Likes relation Migration created

---
+++ Migrations/20240711171833_UserLikeRelationCreated.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PostableRESTFulApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserLikeRelationCreated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Likes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Likes_UserId",
+                table: "Likes",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Likes_Users_UserId",
+                table: "Likes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Likes_Users_UserId",
+                table: "Likes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Likes_UserId",
+                table: "Likes");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Likes");
+        }
+    }
+}
