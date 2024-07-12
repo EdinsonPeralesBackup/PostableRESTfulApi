4bd095ba368ae5a7e18d26a1eed3b8624270c8d4
Edinson Perales <eperalesvi@unprg.edu.pe>
2024-07-12 18:53:39 -0500

    DeleteUser endpoint added

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -83,18 +83,11 @@ namespace PostableRESTfulApi.Controllers
             return Ok(new { message = "User Actualizado.", user });                          
         }
 
+        // DELETE: api/me
         [Authorize]
-        [HttpPost("{postId}/like")]
-        public async Task<IActionResult> GiveLike(int postId)
+        [HttpDelete("")]
+        public async Task<IActionResult> DeleteLike()
         {
-            var post = await _context.Posts.FindAsync(postId);            
-
-            if (post == null)
-            {
-                return NotFound($"ERROR: El post con id {postId} no existe!!");
-                throw new InvalidOperationException();
-            } 
-
             var userId = User.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier")?.Value;
             if (userId == null)
             {
@@ -107,75 +100,7 @@ namespace PostableRESTfulApi.Controllers
                 return Unauthorized("El usuario autenticado no existe en el sistema.");
             }
 
-            var like = new Like
-            {
-                Post = post,
-                User = user,
-                CreatedAt = DateTime.Now
-            };            
-
-            _context.Likes.Add(like);
-            
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateException ex)
-            {
-                if (ex.InnerException is SqlException sqlException && sqlException.Number == 2627)
-                {
-                    // Error 2627 es para violación de clave única en SQL Server
-                    return BadRequest("El usuario ya ha dado like a este post.");
-                }
-                else
-                {
-                    // Manejar otros errores de actualización de base de datos
-                    return StatusCode(500, "Ocurrió un error al registrar el like.");
-                }
-            }
-
-            var likeResponse = new 
-            {
-                id = like.Id,
-                content = post.Content,
-                createdAt = like.CreatedAt,
-                username = user.UserName,
-                likesCount = like.Post.Likes.Count
-            };
-            return Ok(new { message = "Like registrado.", likeResponse });         
-        }
-
-        [Authorize]
-        [HttpDelete("{postId}/like")]
-        public async Task<IActionResult> DeleteLike(int postId)
-        {
-            var post = await _context.Posts.FindAsync(postId); 
-
-            if (post == null)
-            {
-                return NotFound($"El post con id {postId} no existe!!!");
-            }
-
-            var userId = User.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier")?.Value;
-            if (userId == null)
-            {
-                return Unauthorized("No se encontro el id del usuario autenticado");
-            }
-
-            var user = await _context.Users.FindAsync(int.Parse(userId));
-            if (user == null)
-            {
-                return Unauthorized("El usuario autenticado no existe en el sistema.");
-            }
-
-            var like = await _context.Likes
-                .FirstOrDefaultAsync(l => l.UserId == user.Id && l.PostId == post.Id);
-            if (like == null)
-            {
-                return NotFound($"El post no tenía like anteriormente!!!");
-            }
-
-            _context.Remove(like);
+            _context.Remove(user);
             try
             {
                 await _context.SaveChangesAsync();
@@ -184,7 +109,7 @@ namespace PostableRESTfulApi.Controllers
             {
                 return BadRequest($"ERROR: {ex.Message}");
             }
-            return Ok(new { message = "Like Eliminado."});
+            return NoContent();
         }
     }
 }
