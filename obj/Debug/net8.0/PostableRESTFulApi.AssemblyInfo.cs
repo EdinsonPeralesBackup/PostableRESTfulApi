e79a852652547289bb934754bbc428c54c783375
Edinson Perales <eperalesvi@unprg.edu.pe>
2024-07-11 20:42:48 -0500

    Load Like data in LikeConfig.cs

--- obj/Debug/net8.0/PostableRESTFulApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/PostableRESTFulApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PostableRESTFulApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afdcf180991978ebed2e8967f73d620c8e60627a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b3e52c6dcb8f7f73c82b4466aa5b7cfe2bab998")]
 [assembly: System.Reflection.AssemblyProductAttribute("PostableRESTFulApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PostableRESTFulApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
