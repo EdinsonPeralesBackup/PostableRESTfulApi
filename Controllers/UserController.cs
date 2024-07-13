043a400800e312d3ec9648ca1f6fc0b30b4cdd9d
Edinson Perales <eperalesvi@unprg.edu.pe>
2024-07-12 23:48:54 -0500

    Adding personalazing error messages in UserController

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using PostableRESTfulApi.Controllers.Error;
 using PostableRESTfulApi.Data;
 using PostableRESTfulApi.Models;
 using PostableRESTfulApi.Models.DTOs.Post;
@@ -28,13 +29,13 @@ namespace PostableRESTfulApi.Controllers
             var userId = User.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier")?.Value;
             if (userId == null)
             {
-                return Unauthorized("No se encontro el id del usuario autenticado");
+                return ErrorHelper.ErrorResponse(this, 401, "Unauthorized", "No se encontro el id del usuario autenticado");
             }
 
             var user = await _context.Users.FindAsync(int.Parse(userId));
             if (user == null)
             {
-                return Unauthorized("El usuario autenticado no existe en el sistema.");
+                return ErrorHelper.ErrorResponse(this, 401, "Unauthorized", "El usuario autenticado no existe en el sistema.");                
             }
             return Ok(user);
         }
@@ -47,13 +48,13 @@ namespace PostableRESTfulApi.Controllers
             var userId = User.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier")?.Value;
             if (userId == null)
             {
-                return Unauthorized("No se encontro el id del usuario autenticado");
+                return ErrorHelper.ErrorResponse(this, 401, "Unauthorized", "No se encontro el id del usuario autenticado");
             }
 
             var user = await _context.Users.FindAsync(int.Parse(userId));
             if (user == null)
             {
-                return Unauthorized("El usuario autenticado no existe en el sistema.");
+                return ErrorHelper.ErrorResponse(this, 401, "Unauthorized", "El usuario autenticado no existe en el sistema.");                
             }
 
             if (updateUserDto.Email != null)
@@ -77,7 +78,7 @@ namespace PostableRESTfulApi.Controllers
             }
             catch (DbUpdateException ex)
             {
-                return BadRequest($"ERROR: {ex.Message}");
+                return ErrorHelper.ErrorResponse(this, 500, "Internal Server Error", ex.Message);
             }
         
             return Ok(new { message = "User Actualizado.", user });                          
@@ -91,13 +92,13 @@ namespace PostableRESTfulApi.Controllers
             var userId = User.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier")?.Value;
             if (userId == null)
             {
-                return Unauthorized("No se encontro el id del usuario autenticado");
+                return ErrorHelper.ErrorResponse(this, 401, "Unauthorized", "No se encontro el id del usuario autenticado");
             }
 
             var user = await _context.Users.FindAsync(int.Parse(userId));
             if (user == null)
             {
-                return Unauthorized("El usuario autenticado no existe en el sistema.");
+                return ErrorHelper.ErrorResponse(this, 401, "Unauthorized", "El usuario autenticado no existe en el sistema.");                
             }
 
             _context.Remove(user);
@@ -107,7 +108,7 @@ namespace PostableRESTfulApi.Controllers
             }
             catch (DbUpdateException ex)
             {
-                return BadRequest($"ERROR: {ex.Message}");
+                return ErrorHelper.ErrorResponse(this, 500, "Internal Server Error", ex.Message);
             }
             return NoContent();
         }
